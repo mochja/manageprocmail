94a5a2ff9b0c15097c8e82bd7d423ad1a1d1bc85
Ján Mochňak <janmochnak@icloud.com>
2019-03-10 22:14:39 +0100

    prototype

---
+++ manageprocmail.js
@@ -0,0 +1,55 @@
+  if (window.rcmail) {
+    rcmail.addEventListener('init', function(evt) {
+    if (rcmail.env.action.startsWith('plugin.manageprocmail')) {
+
+
+
+      if (rcmail.gui_objects.filterslist) {
+      // alert('ok');
+
+        rcmail.filters_list = new rcube_list_widget(rcmail.gui_objects.filterslist,
+          {multiselect:false, draggable:true, keyboard:true});
+
+        rcmail.filters_list
+          .addEventListener('select', function(e) { rcmail.manageprocmail_select(e); })
+          //.addEventListener('dragstart', function(e) { rcmail.manageprocmail_dragstart(e); })
+          //.addEventListener('dragend', function(e) { rcmail.manageprocmail_dragend(e); })
+          .addEventListener('initrow', function(row) {
+            row.obj.onmouseover = function() { rcmail.manageprocmail_focus_filter(row); };
+            row.obj.onmouseout = function() { rcmail.manageprocmail_unfocus_filter(row); };
+          })
+          .init();
+      }
+    }
+    })
+  }
+
+// Filter selection
+rcube_webmail.prototype.manageprocmail_select = function(list)
+{
+  var id = list.get_single_selection();
+
+  if (id != null) {
+    id = list.rows[id].uid;
+    this.load_manageprocmailframe('_fid=' + id);
+  }
+
+  var has_id = typeof(id) != 'undefined' && id != null;
+  this.enable_command('plugin.manageprocmail-act', 'plugin.manageprocmail-del', has_id);
+};
+
+// load filter frame
+rcube_webmail.prototype.load_manageprocmailframe = function(add_url, reset)
+{
+  // if (reset)
+    // this.reset_filters_list();
+
+  if (this.env.contentframe && window.frames && window.frames[this.env.contentframe]) {
+    var lock = this.set_busy(true, 'loading');
+
+    target = window.frames[this.env.contentframe];
+    target.location.href = this.env.comm_path
+      + '&_action=plugin.manageprocmail-action&_framed=1&_unlock=' + lock
+      + (add_url ? ('&' + add_url) : '');
+  }
+};
--- manageprocmail.php
+++ manageprocmail.php
@@ -8,6 +8,9 @@ class manageprocmail extends rcube_plugin
     $this->rc = rcube::get_instance();
 
     $this->register_action('plugin.manageprocmail', array($this, 'manageprocmail_actions'));
+    
+    $this->register_action('plugin.manageprocmail-action', array($this, 'manageprocmail_actions2'));
+
 
     if ($this->rc->task == 'settings') {
         $this->add_hook('settings_actions', array($this, 'settings_actions'));
@@ -27,17 +30,49 @@ class manageprocmail extends rcube_plugin
     ]);
   }
 
+  function formedit($attrib) {
+    $attrib += ['id' => '123asdfasd'];
+
+        $input = new html_inputfield(array('type' => 'text', 'name' => '_name',
+            'id' => 'rcmanageprocmalx', 'size' => 30));
+
+         $this->rc->output->add_gui_object('filterform', $attrib['id']);
+
+        $out = $this->rc->output->form_tag(array(
+            'action'  => $this->rc->url(array('action' => $this->rc->action, 'a' => 'import')),
+            'method'  => 'post',
+            'enctype' => 'multipart/form-data') + $attrib,
+            $input->show()
+        );
+
+        return $out;
+  }
+
+  function manageprocmail_actions2() {
+
+            $this->rc->output->add_handlers(array(
+      'filterform'      => array($this, 'formedit')));
+
+      $this->rc->output->send('manageprocmail.filteredit');
+  }
+
   function manageprocmail_actions() {
     $this->rc->output->add_handlers(array(
       'filterslist'      => array($this, 'filters_list'),
       'filterframe'      => array($this, 'filter_frame'),
     ));
 
+            // include main js script
+        if ($this->rc->output->type == 'html') {
+            $this->include_script('manageprocmail.js');
+        }
+
     $result = $this->list_rules();
 
-    $this->rc->output->command('manageprocmail_listupdate', 'list', array('list' => $result));
+    // $this->rc->output->command('manageprocmail_listupdate', 'list', array('list' => $result));
     // $this->rc->output->set_pagetitle($this->plugin->gettext('filters'));
     $this->rc->output->send('manageprocmail.manageprocmail');
+
   }
 
     function filter_frame($attrib)
@@ -51,6 +86,9 @@ class manageprocmail extends rcube_plugin
         $result = $this->list_rules();
 
         $out = $this->rc->table_output($attrib, $result, $a_show_cols, 'id');
+        $this->rc->output->add_gui_object('filterslist', $attrib['id']);
+            $this->rc->output->include_script('list.js');
+
 
         return $out;
   }
