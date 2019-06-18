95c660b64d27aafcc584978a86a0b2c3c8b201f7
Ján Mochňak <janmochnak@gmail.com>
2019-06-18 13:39:07 +0200

    fix: installation with db prefix

--- SQL/mysql.initial.sql
+++ SQL/mysql.initial.sql
@@ -1,4 +1,3 @@
--- Create syntax for TABLE 'manageprocmail_filters'
 CREATE TABLE `manageprocmail_filters` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
@@ -10,11 +9,9 @@ CREATE TABLE `manageprocmail_filters` (
   `created` datetime DEFAULT '1000-01-01 00:00:00',
   `enabled` int(11) DEFAULT '1',
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `manageprocmail_filters_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  KEY `user_id` (`user_id`)
+) /*!40000 ENGINE=InnoDB */ DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Create syntax for TABLE 'manageprocmail_rules'
 CREATE TABLE `manageprocmail_rules` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `filter_id` int(11) unsigned NOT NULL,
@@ -22,11 +19,10 @@ CREATE TABLE `manageprocmail_rules` (
   `op` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '',
   `against` text CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `filter_id` (`filter_id`),
-  CONSTRAINT `manageprocmail_rules_ibfk_1` FOREIGN KEY (`filter_id`) REFERENCES `manageprocmail_filters` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  KEY `filter_id` (`filter_id`)
+  -- CONSTRAINT `manageprocmail_rules_ibfk_1` FOREIGN KEY (`filter_id`) REFERENCES `manageprocmail_filters`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) /*!40000 ENGINE=InnoDB */ DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Create syntax for TABLE 'manageprocmail_vacations'
 CREATE TABLE `manageprocmail_vacations` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
@@ -39,6 +35,5 @@ CREATE TABLE `manageprocmail_vacations` (
   `days` int(11) NOT NULL DEFAULT '7',
   `enabled` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `manageprocmail_vacations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  KEY `user_id` (`user_id`)
+) /*!40000 ENGINE=InnoDB */ DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- manageprocmail.php
+++ manageprocmail.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-
 \Tracy\Debugger::enable(false);
 
 function cToIngo($field)
